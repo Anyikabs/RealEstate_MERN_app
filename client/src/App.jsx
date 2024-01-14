6be34de2be633006d82f7be9908a1acc6c1a2e59
Anyikabs <114746509+Anyikabs@users.noreply.github.com>
2024-01-14 07:55:26 +0100

    create pages and routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,13 +4,6 @@ import About from './pages/About';
 import Profile from './pages/Profile';
 import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
-import React from 'react';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from './pages/Home';
-import About from './pages/About';
-import Profile from './pages/Profile';
-import SignIn from './pages/SignIn';
-import SignUp from './pages/SignUp';
 
 export default function App() {
   return (
